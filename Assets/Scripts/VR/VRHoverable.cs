0245bf040b8f48f5ca3203b7367220101f28bbf8
rogeto20 <rogeto20@jcu.edu>
2019-10-07 16:08:02 -0400

    Added updated gitignore file

---
+++ Assets/Scripts/VR/VRHoverable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ===============================
+// AUTHOR: Craig Milby
+// DATE: 25 October, 2017
+// PURPOSE: Hovereable calls unity events when a hover starts and ends. This allows
+//			us to have different interactions simply when "selecting" it.
+// ===============================
+public class VRHoverable : MonoBehaviour {
+
+	public UnityEngine.Events.UnityEvent HoverStart;
+	public UnityEngine.Events.UnityEvent HoverEnd;
+
+	[HideInInspector]
+	public bool IsHovering = false;
+
+	void Start ( ) {
+		
+	}
+
+	void Update ( ) {
+		
+	}
+
+	/// <summary>
+	/// Call a unity event when hovering begins
+	/// 
+	/// This is called from interactable
+	/// UnityEvents are handled by Unity internally
+	/// </summary>
+	public void HoverStarted ( ) {
+		HoverStart.Invoke ( );
+		IsHovering = true;
+	}
+
+	/// <summary>
+	/// Call a unity event when hovering ends
+	/// 
+	/// This is called from interactable
+	/// UnityEvents are handled by Unity internally
+	/// </summary>
+	public void HoverEnded ( ) {
+		HoverEnd.Invoke ( );
+		IsHovering = false;
+	}
+}
---
+++ Assets/Scripts/VR/VRLauncherLaunchButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// AUTHOR: Craig Milby
+/// DATE: 30 October, 2017
+/// PURPOSE: Handles the functionality of pressing the launch
+/// 		 button in the VR launcher scene
+/// </summary>
+public class VRLauncherLaunchButton : MonoBehaviour {
+
+	public VRLauncherLoadMoleculeButton LoadMolecules;
+
+	public void HandleLaunchButtonPressed ( ) {
+		List<string> molecules = LoadMolecules.LoadMolecules;
+
+
+		for ( int i = 0; i < molecules.Count; i++ ) {
+			SceneManager.SetParameter ( i.ToString ( ), molecules [ i ] );
+		}
+
+		SceneManager.Load ( "VRScene", true );
+	}
+}
+
